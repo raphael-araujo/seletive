a9069a0ab05f30246cfad0258724f5660b5f5d48
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-11-09 18:46:48 -0300

    adicionando a view e url para a página nova_empresa

--- empresa/views.py
+++ empresa/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import redirect, render,  HttpResponse
 
-# Create your views here.
+
+def nova_empresa(request):
+    return render(request, 'nova_empresa.html')
