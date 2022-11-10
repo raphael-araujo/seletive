78514a7e10a3fa381217fa9294c70fcbbe951693
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-11-10 20:03:16 -0300

    atualizando a view 'nova_empresa'

--- empresa/views.py
+++ empresa/views.py
@@ -1,6 +1,55 @@
-# from django.http import HttpResponse
-from django.shortcuts import redirect, render,  HttpResponse
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.shortcuts import HttpResponse, redirect, render
+
+from .models import Empresa, Tecnologia
+from .utils import empresa_is_valid
 
 
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        cidade = request.POST['cidade']
+        endereco = request.POST['endereco']
+        nicho = request.POST['nicho']
+        tecnologias = request.POST.getlist('tecnologias')
+        caracteristicas = request.POST['caracteristicas']
+        logo = request.FILES.get('logo')
+        print(f'1- {tecnologias}')
+        if not empresa_is_valid(request, nome, email, cidade, endereco, nicho, tecnologias, caracteristicas, logo):
+            return redirect('/home/nova_empresa')
+        try:
+            e = Empresa(
+                logo=logo,
+                nome=nome,
+                email=email,
+                cidade=cidade,
+                endereco=endereco,
+                nicho_mercado=nicho,
+                caracteristica_empresa=caracteristicas
+            )
+            e.save()
+            e.tecnologias.add(*tecnologias)
+            e.save()
+
+            messages.add_message(
+                request,
+                level=constants.SUCCESS,
+                message='Empresa cadastrada com sucesso.'
+            )
+            return redirect('/home/empresas')
+        except:
+            messages.add_message(
+                request,
+                level=constants.ERROR,
+                message='Erro interno do sistema'
+            )
+            return redirect('/home/nova_empresa')
+
+    else:
+        techs = Tecnologia.objects.all()
+        context = {
+            'techs': techs
+        }
+        return render(request, 'nova_empresa.html', context)
