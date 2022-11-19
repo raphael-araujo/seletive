721dcbf639ba92a832b0e41558388cf0cd816ff0
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-11-19 20:41:35 -0300

    adicionando o método 'progresso' no modelo Vaga

--- empresa/models.py
+++ empresa/models.py
@@ -58,5 +58,10 @@ class Vaga(models.Model):
     tecnologias_estudar = models.ManyToManyField(
         Tecnologia, related_name='estudar')
 
+    def progresso(self):
+        x = [((i+1)*20, j[0]) for i, j in enumerate(self.choices_status)]
+        x = list(filter(lambda x: x[1] == self.status, x))[0][0]
+        return x
+
     def __str__(self):
         return self.titulo
